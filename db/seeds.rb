de2e777cc8a5423007b8fff839172ddbe021bd0a
Horia Radu <horia.radu23@gmail.com>
2017-01-10 12:23:48 +0200

    Some UI stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,18 @@
   { code: 'R0501', authors: 'Felix Buffiere', title: 'Miturile lui Homer si gandirea greaca', location: 'Raft 05',
     year: 1987, publisher: 'Univers' },
   { code: 'R1503', authors: 'Radu Paraschivescu', title: 'Racani, pifani si veterani', location: 'Raft 15',
+    year: 2008, publisher: 'Humanitas' },
+  { code: 'R0502', authors: 'Felix Buffiere', title: 'Miturile lui Homer si gandirea greaca', location: 'Raft 05',
+    year: 1987, publisher: 'Univers' },
+  { code: 'R1504', authors: 'Radu Paraschivescu', title: 'Racani, pifani si veterani', location: 'Raft 15',
+    year: 2008, publisher: 'Humanitas' },
+  { code: 'R0503', authors: 'Felix Buffiere', title: 'Miturile lui Homer si gandirea greaca', location: 'Raft 05',
+    year: 1987, publisher: 'Univers' },
+  { code: 'R1505', authors: 'Radu Paraschivescu', title: 'Racani, pifani si veterani', location: 'Raft 15',
+    year: 2008, publisher: 'Humanitas' },
+  { code: 'R0504', authors: 'Felix Buffiere', title: 'Miturile lui Homer si gandirea greaca', location: 'Raft 05',
+    year: 1987, publisher: 'Univers' },
+  { code: 'R1506', authors: 'Radu Paraschivescu', title: 'Racani, pifani si veterani', location: 'Raft 15',
     year: 2008, publisher: 'Humanitas' }
 ].each do |data|
   Book.create(data)
