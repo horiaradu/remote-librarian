be0a9becdca9f26f7393e28fd4d055bc297a6b52
Horia Radu <horia.radu23@gmail.com>
2017-01-12 09:01:31 +0200

    Add notices Fix document on load with turbolinks

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,12 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function () {
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+document.addEventListener("turbolinks:load", function () {
   $('.modal').modal();
 });
 
 $(document).on('click', '.delete-book-btn', function openDeleteConfirmationModal(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
   var bookId = $(this).data('id');
   var bookTitle = $(this).data('title');
   $("#delete-book-link").attr('href', '/books/' + bookId);
