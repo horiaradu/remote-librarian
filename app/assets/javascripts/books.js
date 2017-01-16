67e5d415dfec03c86c8bf3b346b3cd0db0bf543b
Horia Radu <horia.radu23@gmail.com>
2017-01-16 21:05:27 +0200

    Add search in navbar

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -3,6 +3,18 @@
 
 document.addEventListener("turbolinks:load", function () {
   $('.modal').modal();
+  $('#search-bar').hide();
+
+  $('#search-close').click(function hideSearchBar() {
+    $('#default-navbar').show();
+    $('#search-bar').hide();
+  });
+
+  $('#navbar-search-btn').click(function showSearchBar() {
+    $('#default-navbar').hide();
+    $('#search-bar').show();
+    $('#search').focus();
+  });
 });
 
 $(document).on('click', '.delete-book-btn', function openDeleteConfirmationModal(event) {
