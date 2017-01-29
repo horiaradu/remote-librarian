101d965550bcfb5597498e2789b9a5a8e7937009
Horia Radu <horia.radu23@gmail.com>
2017-01-29 10:49:08 +0200

    Confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129072751) do
+ActiveRecord::Schema.define(version: 20170129083918) do
 
   create_table "books", force: :cascade do |t|
     t.string   "code"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 20170129072751) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
